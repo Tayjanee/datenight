6aa11309f177ff1bd1488144c831673b50463ea6
Tayjanee <tm.mathis16@gmail.com>
2022-12-20 22:40:19 -0600

    edits

--- datenight/src/App.js
+++ datenight/src/App.js
@@ -1,29 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Movies from "./components/Movies";
-import { BrowserRouter as Routes, Route, Link } from "react-router-dom";
-import NavBar from "../pages/NavBar";
-import Home, { Banner } from "../pages/Home";
-import About from "../pages/About";
-
-// const Movies = () => {
-//   const [movies, setMovies] = useState([]);
-
-//   useEffect(() => {
-//     let key = "e7cd0eac";
-
-//     const moviesData = async () => {
-//       let response = await fetch(
-//         `http://www.omdbapi.com/?s=date&apikey=${key}`
-//       );
-
-//       let data = await response.json();
-//       console.log(data.Search);
-//       setMovies(data.Search);
-//     };
-//     moviesData();
-//   }, []);
-//   return <Movies movies={movies} />;
-// };
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -33,7 +13,7 @@ const App = () => {
 
     const moviesData = async () => {
       let response = await fetch(
-        `http://www.omdbapi.com/?s=date&apikey=${key}`
+        `http://www.omdbapi.com/?s=love&apikey=${key}`
       );
 
       let data = await response.json();
@@ -42,31 +22,14 @@ const App = () => {
     };
     moviesData();
   }, []);
-  return <Movies movies={movies} />;
+
+  return (
+    <Routes>
+      <Route path="/" element={<Movies movies={movies} />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="NavBar" element={<NavBar />} />
+    </Routes>
+    );
 };
-
-<Router>
-  <NavBar />
-  <div>
-    <Link to="/"> Home</Link>
-    <Link to="/About"> About</Link>
-    <Link to="/Movies"> Movies</Link>
-  </div>
-
-  <Banner />
-
-  <Routes>
-    <Route path="/" exact>
-      <Home />
-    </Route>
-
-    <Route path="/about" exact>
-      <About />
-    </Route>
-    <Route path="/movies" exact>
-      <Movies />
-    </Route>
-  </Routes>
-</Router>;
-
 export default App;
